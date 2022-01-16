5a10d3256d1921526e1ddcf6751a4146d9bb498e
Maxim Chernyshov <dsgnrvd@gmail.com>
2022-01-16 21:38:10 +0300

    @designervoid/ton-design-system update to 0.0.3

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 // @ts-ignore
-import {registerCustomElements} from '@designervoid/ton-design-system/dist/index.es.js';
+import {registerCustomElements, getAllCustomElementsNames} from '@designervoid/ton-design-system/dist/index.es.js';
 import '@designervoid/ton-design-system/src/assets/styles/global.css';
 
 registerCustomElements();
+getAllCustomElementsNames();
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
-  <button-primary></button-primary>
-  <spacing-block type="top" payload="12"></spacing-block>
-  <button-outline></button-outline>
-  <spacing-block type="top" payload="12"></spacing-block>
+  <layout-element>
+  <main slot="content">
+    <button-primary></button-primary>
+    <spacing-block type="top" payload="12"></spacing-block>
+    <button-outline></button-outline>
+    <spacing-block type="top" payload="12"></spacing-block>
+  </main>
+  </layout-element>
 `
